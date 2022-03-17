893f5f3c98dd38ea69edd115d4be4e2f9085cb8b
ivobul <24499940+ivobul@users.noreply.github.com>
2022-03-17 14:16:06 -0400

    Add files via upload

---
+++ faq-accordion-card/script.js
@@ -0,0 +1,11 @@
+const question = document.querySelectorAll(".card__body-question");
+const answer = document.querySelectorAll(".card__body-answer");
+const arrow = document.querySelectorAll(".icon");
+
+
+question.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+    item.classList.toggle("card__body-question--clicked");
+  });
+});
